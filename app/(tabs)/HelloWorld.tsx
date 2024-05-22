b92188193a00521a0bc71e6f81e2a0cdde3400df
rickyasuncion <ricky.asuncion@edu.sait.ca>
2024-05-22 09:26:16 -0600

    lab1

---
+++ app/(tabs)/HelloWorld.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function HelloWorld() {
+  return (
+    <View>
+      <Text style={{fontSize: 60}}>Hello, World!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
